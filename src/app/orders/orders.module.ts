bfe5cbbb3ccb8bf235bfca17457370ceab50bc0c
dylanmeuriot <dylanmeuriot@gmail.com>
2021-12-06 10:22:37 +0100

    Commit 6 : UiComponent html / scss

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -1,12 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { OrdersRoutingModule } from './orders-routing.module';
-import { PageListOrdersComponent } from './pages/page-list-orders/page-list-orders.component';
 import { PageAddOrderComponent } from './pages/page-add-order/page-add-order.component';
 import { PageEditOrderComponent } from './pages/page-edit-order/page-edit-order.component';
+import { PageListOrdersComponent } from './pages/page-list-orders/page-list-orders.component';
 
 @NgModule({
-  declarations: [PageListOrdersComponent, PageAddOrderComponent, PageEditOrderComponent],
+  declarations: [
+    PageListOrdersComponent,
+    PageAddOrderComponent,
+    PageEditOrderComponent,
+  ],
   imports: [CommonModule, OrdersRoutingModule],
 })
 export class OrdersModule {}
--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,8 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { UiComponent } from './components/ui/ui.component';
+import { Ui2Component } from './components/ui2/ui2.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UiComponent,
+    Ui2Component
+  ],
   imports: [CommonModule],
+  exports: [
+    UiComponent,
+    Ui2Component
+  ],
 })
 export class UiModule {}
