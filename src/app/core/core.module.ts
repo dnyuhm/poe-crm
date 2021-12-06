7c8e9aa03b3a93245055a54b148d776ce4582189
dylanmeuriot <dylanmeuriot@gmail.com>
2021-12-06 14:21:24 +0100

    Commit 7 : la transclusion

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,10 +4,21 @@ import { IconsModule } from '../icons/icons.module';
 import { LoginModule } from '../login/login.module';
 import { TemplatesModule } from '../templates/templates.module';
 import { UiModule } from '../ui/ui.module';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, NavComponent, FooterComponent],
   imports: [CommonModule],
-  exports: [IconsModule, LoginModule, UiModule, TemplatesModule],
+  exports: [
+    IconsModule,
+    LoginModule,
+    UiModule,
+    TemplatesModule,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+  ],
 })
 export class CoreModule {}
--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -4,14 +4,8 @@ import { UiComponent } from './components/ui/ui.component';
 import { Ui2Component } from './components/ui2/ui2.component';
 
 @NgModule({
-  declarations: [
-    UiComponent,
-    Ui2Component
-  ],
+  declarations: [UiComponent, Ui2Component],
   imports: [CommonModule],
-  exports: [
-    UiComponent,
-    Ui2Component
-  ],
+  exports: [UiComponent, Ui2Component],
 })
 export class UiModule {}
