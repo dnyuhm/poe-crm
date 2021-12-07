e024703300a96a40a3314ecfa9d905ce02141ef0
dylanmeuriot <dylanmeuriot@gmail.com>
2021-12-07 10:34:16 +0100

    Commit 8 : ngclass/event binding

--- src/app/ui/components/ui/ui.component.ts
+++ src/app/ui/components/ui/ui.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ui',
   templateUrl: './ui.component.html',
-  styleUrls: ['./ui.component.scss']
+  styleUrls: ['./ui.component.scss'],
 })
-export class UiComponent implements OnInit {
+export class UiComponent {
+  public close: boolean = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  public toggle(): void {
+    this.close = !this.close;
+    console.log(this.close);
   }
-
 }
