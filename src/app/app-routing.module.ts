d8bbdcdbcc20d70279cf84ed4576488a07b36d7e
dylanmeuriot <dylanmeuriot@gmail.com>
2021-12-09 10:35:19 +0100

    Commit 17.1 et 18  : tp routing et json server

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { PageSignInComponent } from './login/pages/page-sign-in/page-sign-in.component';
-import { PageSignUpComponent } from './login/pages/page-sign-up/page-sign-up.component';
 
 const routes: Routes = [
-  { path: 'sign-in', component: PageSignInComponent },
-  { path: 'sign-up', component: PageSignUpComponent },
   { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
   {
     path: 'orders',
     loadChildren: () =>
       import('src/app/orders/orders.module').then((m) => m.OrdersModule),
+  },
+  {
+    path: 'clients',
+    loadChildren: () =>
+      import('src/app/clients/clients.module').then((m) => m.ClientsModule),
+  },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('src/app/page-not-found/page-not-found.module').then(
+        (m) => m.PageNotFoundModule
+      ),
   },
 ];
 
